25d3c7003864ec9c7fc4ec5a364e096207c1eb24
Marco <marco.xausa@studenti.unitn.it>
2024-03-26 00:46:13 +0100

    Fixed spaces in DelegationStorage3.sol

--- truffle/contracts/DelegationStorage3.sol
+++ truffle/contracts/DelegationStorage3.sol
@@ -1,34 +1,33 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract DelegationStorage3{
+contract DelegationStorage3 {
 
-    struct Service{
-        mapping(address=>bool) isDelegated;         
+
+    struct Service {
+        mapping(address => bool) isDelegated;
         address[] delegatedAddresses;
         uint256 delegationAmount;
     }
 
-    struct Institution{
-        mapping(bytes32=>Service) services;             //All services of an institution
+    struct Institution {
+        mapping(bytes32 => Service) services;       //All services of an institution
+        mapping(bytes32 => bool) servicePresent;
         bytes32[] allServices;
-        mapping(bytes32=>bool) servicePresent;
-    } 
+    }
 
-    //users' delegations 
-    mapping( address=> mapping(address=>Institution ) )  users;
+    //users' delegations
+    mapping(address => mapping(address => Institution)) users;
 
-    //all the allowed users and institutions 
-    mapping(address=>bool)  authorizedUsers;
-    mapping(address=>bool)  authorizedInstitutions;
-
+    //all the allowed users and institutions
+    mapping(address => bool) authorizedUsers;
+    mapping(address => bool) authorizedInstitutions;
 
     //services offered by each institution
-    mapping(address=>mapping(bytes32=> bool)) institutionServices;
-
+    mapping(address => mapping(bytes32 => bool)) institutionServices;
 
     //struct for returning data to frontend
-    struct returnValue{
+    struct returnValue {
         address delegated;
         bytes32 services;
     }
@@ -36,15 +35,19 @@ contract DelegationStorage3{
     //only account allowed to add users
     address owner;
 
-    constructor() { //runs when the contract is deployed
-        owner = msg.sender; //the owner of the contract is the address that deploys it 
+    constructor() {
+        //runs when the contract is deployed
+        owner = msg.sender; //the owner of the contract is the address that deploys it
     }
 
-    function hash(string memory a) internal pure returns(bytes32) {
+    function hash(string memory a) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked((a)));
     }
 
-    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+    function compareStrings(
+        string memory a,
+        string memory b
+    ) internal pure returns (bool) {
         return (hash(a) == hash(b));
     }
 }
